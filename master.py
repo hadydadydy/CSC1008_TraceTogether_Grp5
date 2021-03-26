bf239522a8599a74c0a2c26488e565952a88fb94
zheiweii <zheiweilaw@gmail.com>
2021-03-26 19:45:12 +0800

    changed your excel namesssss

--- master.py
+++ master.py
@@ -1,7 +1,7 @@
 from pandas import read_excel
 
-my_sheet = 'Dataset 1' # change it to your sheet name, you can find your sheet name at the bottom left of your excel file
+my_sheet = 'SafeEntry' # change it to your sheet name, you can find your sheet name at the bottom left of your excel file
 file_name = 'dataset.xlsx' # change it to the name of your excel file
 
 df = read_excel(file_name, sheet_name = my_sheet)
-print(len(df["Device ID"])) # shows headers with top 5 rows
+print(len(df["NRIC"])) # shows headers with top 5 rows
