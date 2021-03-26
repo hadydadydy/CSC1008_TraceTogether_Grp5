fdc72a84e02a83011b5934f1e7296cdebdb65d88
Ezra <ezrayeoshua123@gmail.com>
2021-03-26 19:52:08 +0800

    create case class

--- master.py
+++ master.py
@@ -4,4 +4,13 @@ my_sheet = 'Dataset 1' # change it to your sheet name, you can find your sheet n
 file_name = 'dataset.xlsx' # change it to the name of your excel file
 
 df = read_excel(file_name, sheet_name = my_sheet)
-print(len(df["Device ID"])) # shows headers with top 5 rows
+print(df["Device ID"]) # shows headers with top 5 rows
+
+class Case:
+    def __init__(self, nric, location, checkInDate, checkInTime, checkOutTime):
+        self.nric = nric
+        self.location = location
+        self.checkInDate = checkInDate
+        self.checkInTime = checkInTime
+        self.checkOutTime = checkOutTime
+
